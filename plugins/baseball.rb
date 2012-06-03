7c109a27a253819c81033d0b2864d609586d2de8
Jon Eisen <jon.m.eisen@gmail.com>
2012-06-03 09:16:01 -0400

    Debugged the \!baseball plugin

--- plugins/baseball.rb
+++ plugins/baseball.rb
@@ -1,48 +1,52 @@
 require 'uri'
 
 begin
-	require 'teams'
+	require 'team'
 
 class Baseball < Linkbot::Plugin
     def self.on_message(message, match)
-		if match.length == 0
+	def on_message(match)
+		if !match || match.length < 2
 			"!baseball [team] [date]\nTeams: #{Team::teams.keys.join(' ')}\nDate Format: DD-MM-YYYY or today (default) or yesterday"
 		else
 			date = [Date.today.year, Date.today.month, Date.today.day]
-			if match.length == 2
-				if match[1] == "yesterday"
-					date = [Date.yesterday.year, Date.yesterday.month, Date.yesterday.day]
-				elsif match[1] =~ /(\d{2})-(\d{2})-(\d{4})/
-					dmatch = match[1].match(/(\d{2})-(\d{2})-(\d{4})/)
+			if match.length == 3
+				if match[2] == "yesterday"
+					date = [(Date.today - 1).year, (Date.today - 1).month, (Date.today - 1).day]
+				elsif match[2] =~ /(\d{2})-(\d{2})-(\d{4})/
+					dmatch = match[2].match(/(\d{2})-(\d{2})-(\d{4})/)
 					date = [dmatch[0],dmatch[1],dmatch[2]]
-				elsif match[1] != "today"
-					"ERROR: Date should be DD-MM-YYYY or today or yesterday"
+				elsif match[2] != "today"
+					raise "ERROR: Date should be DD-MM-YYYY or today or yesterday"
 				end
 			end
 	      begin
-				t = Team.new(match[0])
+				puts "Getting linescore for team: #{match[1]} on date: #{date}"
+				t = Team.new(match[1])
 				g = t.games_for_date(date[0],date[1],date[2])
 				s = ''
 				g.each { |h| s += h.print_linescore + "\n" }
-			rescue ArgumentError e
+			rescue ArgumentError => e
 				s = 'ERROR: ' + e
+			rescue
+				s = 'An unknown error occurred!'
 			end
 			s
 		end
     end
 
-    def self.help
+    def help
       "!baseball [team] [date] - Gets baseball scores! (no args gives more help)"
     end
 
     Linkbot::Plugin.register('baseball', self,
       {
-        :message => {:regex => /!baseball (\w*) (.*)/i, :handler => :on_message, :help => :help},
-        :"direct-message" => {:regex => /!baseball (.*)/, :handler => :on_message, :help => :help}
+        :message => {:regex => /!baseball\s*(\w*)\s*(.*)/i, :handler => :on_message, :help => :help},
+        :"direct-message" => {:regex => /!baseball\s*(\w*)\s*(.*)/i, :handler => :on_message, :help => :help}
       }
     )
 end
 
 rescue LoadError
-	puts '!baseball plugin requires gameday_api (http://github.com/timothyf/gameday_api)'
+	puts '!baseball plugin requires gameday_api (sudo gem install gameday_api)'
 end
